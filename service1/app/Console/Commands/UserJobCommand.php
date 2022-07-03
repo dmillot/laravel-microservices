b929df684f1435f49913bd77906a13569528580f
Damien MILLOT <dmillot@pageup.fr>
2022-07-03 20:41:58 +0200

    first commit

---
+++ service1/app/Console/Commands/UserJobCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\UserCreated;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UserJobCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:job';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        UserCreated::dispatch(User::inRandomOrder()->first()->toArray());
+    }
+}
